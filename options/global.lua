0dcb400da66440fcf264153448fc64a0d78836b7
Am-I-Human <andrea.marisio.am@gmail.com>
2024-05-03 08:52:33 +0200

    reading init.lua

---
+++ options/global.lua
@@ -0,0 +1,8 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+vim.g.python3_host_prog = 'C:\\Users\\andre\\.pyenv\\pyenv-win\\versions\\lunarvim3.12\\python.exe'
+vim.g.jukit_mappings = 0
