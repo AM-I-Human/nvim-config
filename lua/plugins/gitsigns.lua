8f4fc35b2ad82baa4791277c972010c82223b2e7
Am-I-Human <andrea.marisio.am@gmail.com>
2024-05-11 18:16:00 +0200

    change in git sings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,45 @@
+icons = require 'ui.icons'
 return {
     {
         'lewis6991/gitsigns.nvim',
         opts = {
+            -- signs = {
+            --     add = { text = '+' },
+            --     change = { text = '~' },
+            --     delete = { text = '_' },
+            --     topdelete = { text = '‾' },
+            --     changedelete = { text = '~' },
             signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
+                add = {
+                    hl = 'GitSignsAdd',
+                    text = icons.ui.BoldLineLeft,
+                    numhl = 'GitSignsAddNr',
+                    linehl = 'GitSignsAddLn',
+                },
+                change = {
+                    hl = 'GitSignsChange',
+                    text = icons.ui.BoldLineLeft,
+                    numhl = 'GitSignsChangeNr',
+                    linehl = 'GitSignsChangeLn',
+                },
+                delete = {
+                    hl = 'GitSignsDelete',
+                    text = icons.ui.Triangle,
+                    numhl = 'GitSignsDeleteNr',
+                    linehl = 'GitSignsDeleteLn',
+                },
+                topdelete = {
+                    hl = 'GitSignsDelete',
+                    text = icons.ui.Triangle,
+                    numhl = 'GitSignsDeleteNr',
+                    linehl = 'GitSignsDeleteLn',
+                },
+                changedelete = {
+                    hl = 'GitSignsChange',
+                    text = icons.ui.BoldLineLeft,
+                    numhl = 'GitSignsChangeNr',
+                    linehl = 'GitSignsChangeLn',
+                },
             },
             on_attach = function(bufnr)
                 local gitsigns = require 'gitsigns'
