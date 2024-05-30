37231bd685cf0ce8660738a09fddf099067a4d1f
Am-I-Human <andrea.marisio.am@gmail.com>
2024-05-30 08:32:39 +0200

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,61 +1,70 @@
 return {
-    'projekt0n/github-nvim-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'bluz71/vim-moonfly-colors',
+    name = 'moonfly',
+    lazy = false,
+    priority = 1000,
     init = function()
-        require('github-theme').setup {
-            options = {
-                -- Compiled file's destination location
-                compile_path = vim.fn.stdpath 'cache' .. '/github-theme',
-                compile_file_suffix = '_compiled', -- Compiled file suffix
-                hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-                hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-                transparent = false, -- Disable setting background
-                terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-                dim_inactive = false, -- Non focused panes set to alternative background
-                module_default = true, -- Default enable value for modules
-                styles = { -- Style to be applied to different syntax groups
-                    comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
-                    functions = 'bold',
-                    keywords = 'italic,bold',
-                    variables = 'NONE',
-                    conditionals = 'NONE',
-                    constants = 'NONE',
-                    numbers = 'NONE',
-                    operators = 'NONE',
-                    strings = 'NONE',
-                    types = 'NONE',
-                },
-                inverse = { -- Inverse highlight for different types
-                    match_paren = false,
-                    visual = false,
-                    search = false,
-                },
-                darken = { -- Darken floating windows and sidebar-like windows
-                    floats = false,
-                    sidebars = {
-                        enabled = true,
-                        list = {}, -- Apply dark background to specific windows
-                    },
-                },
-                modules = { -- List of various plugins and additional options
-                    -- ...
-                },
-            },
-            palettes = {}, --change label like colors
-            specs = {
-                all = {
-                    syntax = {
-                        -- Specs allow you to define a value using either a color or template. If the string does
-                        -- start with `#` the string will be used as the path of the palette table. Defining just
-                        -- a color uses the base version of that color.
-                        variable = 'white',
-                        string = 'green',
-                    },
-                },
-            },
-            groups = {},
-        }
-        vim.cmd 'colorscheme github_dark_high_contrast'
+        vim.cmd [[colorscheme moonfly]]
     end,
-}
+    -- for other options https://github.com/bluz71/vim-moonfly-colors?tab=readme-ov-file
+} -- return {
+--     'projekt0n/github-nvim-theme',
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     init = function()
+--         require('github-theme').setup {
+--             options = {
+--                 -- Compiled file's destination location
+--                 compile_path = vim.fn.stdpath 'cache' .. '/github-theme',
+--                 compile_file_suffix = '_compiled', -- Compiled file suffix
+--                 hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+--                 hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+--                 transparent = false, -- Disable setting background
+--                 terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+--                 dim_inactive = false, -- Non focused panes set to alternative background
+--                 module_default = true, -- Default enable value for modules
+--                 styles = { -- Style to be applied to different syntax groups
+--                     comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
+--                     functions = 'bold',
+--                     keywords = 'italic,bold',
+--                     variables = 'NONE',
+--                     conditionals = 'NONE',
+--                     constants = 'NONE',
+--                     numbers = 'NONE',
+--                     operators = 'NONE',
+--                     strings = 'NONE',
+--                     types = 'NONE',
+--                 },
+--                 inverse = { -- Inverse highlight for different types
+--                     match_paren = false,
+--                     visual = false,
+--                     search = false,
+--                 },
+--                 darken = { -- Darken floating windows and sidebar-like windows
+--                     floats = false,
+--                     sidebars = {
+--                         enabled = true,
+--                         list = {}, -- Apply dark background to specific windows
+--                     },
+--                 },
+--                 modules = { -- List of various plugins and additional options
+--                     -- ...
+--                 },
+--             },
+--             palettes = {}, --change label like colors
+--             specs = {
+--                 all = {
+--                     syntax = {
+--                         -- Specs allow you to define a value using either a color or template. If the string does
+--                         -- start with `#` the string will be used as the path of the palette table. Defining just
+--                         -- a color uses the base version of that color.
+--                         variable = 'white',
+--                         string = 'green',
+--                     },
+--                 },
+--             },
+--             groups = {},
+--         }
+--         vim.cmd 'colorscheme github_dark_high_contrast'
+--     end,
+-- }
