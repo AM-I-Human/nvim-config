c5c942d7214f90e707fa4aff9abf0d3be0930167
Andrea Marisio <andrea.marisio@wefox.com>
2024-05-31 14:01:31 +0200

    roll back theme and adding os featres

--- lua/functions/os.lua
+++ lua/functions/os.lua
@@ -1,6 +1,6 @@
-local M ={}
+local M = {}
 -- Function to detect the operating system
-M.detect_os= function ()
+M.detect_os = function()
     local uname = vim.loop.os_uname()
     if uname.sysname == 'Windows_NT' then
         return 'windows'
@@ -11,8 +11,8 @@ M.detect_os= function ()
     end
 end
 
-os = M.detect_os()
-IS_MAC= 'mac'==os
-IS_WINDOWS= 'windows'==os
-IS_LINUX = 'linux'==os
+CURRENT_OS = M.detect_os()
+IS_MAC = 'mac' == CURRENT_OS
+IS_WINDOWS = 'windows' == CURRENT_OS
+IS_LINUX = 'linux' == CURRENT_OS
 return M
