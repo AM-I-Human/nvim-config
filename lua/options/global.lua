849ba835340474a049d371e209f31ca9ff854bb9
Am-I-Human <andrea.marisio.am@gmail.com>
2024-08-14 10:32:29 +0200

    adding edgy neotree and outline

--- lua/options/global.lua
+++ lua/options/global.lua
@@ -1,15 +1,20 @@
 -- Set <space> as the leader key
+local font = 'OperatorMono Nerd Font'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
 
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.guifont = font .. ':h14'
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+if vim.g.neovide then
+    vim.g.neovide_cursor_vfx_mode = 'wireframe'
+end
+
 if IS_WINDOWS then
     vim.g.python3_host_prog = 'C:\\Users\\andre\\.pyenv\\pyenv-win\\versions\\nvim_3.12.3\\python.exe'
 end
