9edc4e240c69be21e3bfc925b1efcb77b3534615
Am-I-Human <andrea.marisio.am@gmail.com>
2024-08-29 22:15:29 +0200

    add grouped folders

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -191,6 +191,7 @@ return {
             },
             nesting_rules = {},
             filesystem = {
+                scan_mode = 'deep',
                 filtered_items = {
                     visible = false, -- when true, they will just be displayed differently than normal items
                     hide_dotfiles = false,
@@ -223,7 +224,7 @@ return {
                     --               -- the current file is changed while the tree is open.
                     leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
                 },
-                group_empty_dirs = false, -- when true, empty folders will be grouped together
+                group_empty_dirs = true, -- when true, empty folders will be grouped together
                 hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
                 -- in whatever position is specified in window.position
                 -- "open_current",  -- netrw disabled, opening a directory opens within the
