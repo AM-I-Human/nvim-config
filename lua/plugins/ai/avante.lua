fce3d56f4aa8d4690f74884c25dbaa394291fcc6
Am-I-Human <andrea.marisio.am@gmail.com>
2024-12-08 10:23:13 +0100

    fix autocomplete configuration

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -21,31 +21,13 @@ return {
     opts = {
         provider = 'ollama',
         use_absolute_path = true,
+        auto_suggestions_provider = 'ollama',
         vendors = {
             ollama = {
+                __inherited_from = 'openai',
                 api_key_name = '',
-                endpoint = '127.0.0.1:11434/v1',
-                -- model = "codegemma",
-                model = 'codestral:22b',
-                parse_curl_args = function(opts, code_opts)
-                    return {
-                        url = opts.endpoint .. '/chat/completions',
-                        headers = {
-                            ['Accept'] = 'application/json',
-                            ['Content-Type'] = 'application/json',
-                            ['x-api-key'] = 'ollama',
-                        },
-                        body = {
-                            model = opts.model,
-                            messages = require('avante.providers').copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-                            max_tokens = 2048,
-                            stream = true,
-                        },
-                    }
-                end,
-                parse_response_data = function(data_stream, event_state, opts)
-                    require('avante.providers').openai.parse_response(data_stream, event_state, opts)
-                end,
+                endpoint = 'http://localhost:11434/v1',
+                model = 'codegemma',
             },
         },
         behaviour = {
