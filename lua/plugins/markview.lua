0a0c2d9768104c8165a71252242c35d0d870c228
Am-I-Human <andrea.marisio.am@gmail.com>
2025-02-13 22:14:45 +0100

    make avante in the main plugin conf

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,15 +1,13 @@
 return {
     'OXY2DEV/markview.nvim',
-    lazy = false, -- Recommended
+    lazy = false,
     event = 'VeryLazy',
-    -- ft = "markdown" -- If you decide to lazy-load anyway
-
     dependencies = {
-        -- You will not need this if you installed the
-        -- parsers manually
-        -- Or if the parsers are in your $RUNTIMEPATH
         'nvim-treesitter/nvim-treesitter',
-
         'nvim-tree/nvim-web-devicons',
     },
+    opts = {
+        file_types = { 'markdown', 'Avante' },
+    },
+    ft = { 'markdown', 'Avante' },
 }
