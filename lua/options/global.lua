6778556e727a94b7a961f10c5a51f03f18a3df9e
Am-I-Human <andrea.marisio.am@gmail.com>
2025-03-02 21:03:25 +0100

    globals

--- lua/options/global.lua
+++ lua/options/global.lua
@@ -5,14 +5,13 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
--- vim.o.guifont = font .. ':h14'
 
 if vim.g.neovide then
-    vim.g.neovide_cursor_vfx_mode = 'wireframe'
+    vim.o.guifont = font .. ':h10'
 end
 
 if IS_WINDOWS then
